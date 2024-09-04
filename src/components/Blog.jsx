a96017bb832c79d40cb77baa021aeda09d948d37
abrahanalarcon <moiseshazim@gmail.com>
2024-09-03 21:13:18 -0400

    css

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,15 +1,38 @@
-
-import ImagenCalendar from '../img/1.jpg'
+import  { useEffect, useRef } from 'react';
+import ImagenCalendar from '../img/1.jpg';
 
 const Blog = () => {
-    
+    const articleRef = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add('animate-fadeInUp');
+                    }
+                });
+            },
+            {
+                threshold: 0.1, // Se activa la animación cuando el 10% del elemento es visible
+            }
+        );
+
+        if (articleRef.current) {
+            observer.observe(articleRef.current);
+        }
+
+        return () => {
+            if (articleRef.current) {
+                observer.unobserve(articleRef.current);
+            }
+        };
+    }, []);
 
     return (
         <>
             <main className="blog" id="blog">
-                
-               
-                <article className="entrada">
+                <article className="entrada" ref={articleRef}>
                     <div className="entrada__imagen">
                         <picture>
                             <source loading="lazy" srcSet={ImagenCalendar} type="image/webp" />
@@ -18,7 +41,6 @@ const Blog = () => {
                     </div>
                 </article>
             </main>
-            
         </>
     );
 };
